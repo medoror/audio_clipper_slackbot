b1cecf64dcdc53328fe235e7c78f506ee0267d10
Michael Edoror <medoror@gmail.com>
2019-09-14 19:21:16 -0700

    Refactor
    * Add default duration for file upload
    * Extract some logic into custom methods
    * Add usage
    * Cutting mp3 per duration does not seem to be working...
    * Still need to update readme

--- index.js
+++ index.js
@@ -5,47 +5,91 @@ require('dotenv').config();
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 const getInfo = require('ytdl-core');
-const validateURL = require('ytdl-core');
 const request = require('request');
+const shortid = require('shortid');
+const MP3Cutter = require('mp3-cutter');
 
 const app = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     token: process.env.SLACK_BOT_TOKEN
 });
 
+const DEFAULT_DURATION = 10;
+
+function validateYouTubeUrl(potentialUrl) {
+    if (potentialUrl !== undefined || potentialUrl !== '') {
+        const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
+        const match = potentialUrl.match(regExp);
+        if (match && match[2].length === 11) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function generateDuration(duration, defaultDuration) {
+    return !duration || isNaN(parseInt(duration)) ? defaultDuration : duration;
+}
+
+function generateAudioFileName() {
+    return shortid.generate().concat('.mp3');
+}
+
+function clipMp3(audioFilename, duration) {
+    let newCutFilename = '_'.concat(audioFilename);
+    MP3Cutter.cut({
+        src: audioFilename,
+        target: newCutFilename,
+        start: 0,
+        end: duration
+    });
+    return newCutFilename;
+}
+
+function uploadToSlack(title, audioFilename) {
+    request.post({
+        url: 'https://slack.com/api/files.upload',
+        formData: {
+            token: process.env.SLACK_BOT_TOKEN,
+            title: title,
+            filename: audioFilename,
+            filetype: "mp3",
+            channels: "bot-testing",  // TODO: should be replaced with channel in the payload
+            file: fs.createReadStream(audioFilename, {flags: 'r'}),
+        },
+    }, function (err, response) {
+        console.log(JSON.parse(response.body));
+    });
+}
+
+function downloadFromYoutube(title, url, audioFilename, duration) {
+    ytdl(url, {
+        filter: 'audioonly',
+        quality: 'highestaudio',
+
+    }).pipe(fs.createWriteStream(audioFilename, {flags: 'a'}))
+        .on('finish', () => {
+        console.log('wrote all data to file');
+        uploadToSlack(title, clipMp3(audioFilename, duration));
+    });
+}
+
 app.command('/audio', async ({payload, ack, say}) => {
     ack();
-    if (validateURL(payload.text)) {
-        let title;
-        let audioFilename = 'audio.mp3';
-        getInfo(payload.text, (err, info) => {
+    let ytLink = payload.text.split(" ")[0];
+    let duration = generateDuration(payload.text.split(" ")[1], DEFAULT_DURATION)
+
+    if (validateYouTubeUrl(ytLink)) {
+        let title = '';
+        let audioFilename = generateAudioFileName();
+        getInfo(ytLink, (err, info) => {
             if (err) throw err;
             title = info.title;
         });
-
-        ytdl(payload.text, {
-            filter: 'audioonly',
-            quality: 'highestaudio',
-
-        }).pipe(fs.createWriteStream(audioFilename, {flags: 'a'}));
-
-        request.post({
-            url: 'https://slack.com/api/files.upload',
-            formData: {
-                token: process.env.SLACK_BOT_TOKEN,
-                title: payload.text,
-                filename: audioFilename,
-                filetype: "auto",
-                channels: "bot-testing",
-                file: fs.createReadStream(audioFilename),
-            },
-        }, function (err, response) {
-            console.log(JSON.parse(response.body));
-        });
+        downloadFromYoutube(title, ytLink, audioFilename, duration);
 
     } else {
-        say('bad youtube url');
-
+        say("USAGE: /audio [youtube-url] [ duration (DEFAULT_DURATION seconds default) ]")
     }
 });
 
