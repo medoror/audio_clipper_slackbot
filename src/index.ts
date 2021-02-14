25a3456369629db7f31689b6fcdc7c6146c4621f
Michael Edoror <medoror@gmail.com>
2021-02-13 22:00:12 -0800

    Adds serverless scaffolding

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,23 @@ import {Slack} from "./slack";
 import {YoutubeVideo} from "./youtubeVideo";
 import {VideoInterface} from "./videoInterface";
 import {validateYouTubeUrl} from "./youtubeUtils";
-import {App} from "@slack/bolt";
+import {App, ExpressReceiver} from "@slack/bolt";
+// import {awsServerlessExpress} from "aws-serverless-express";
+const awsServerlessExpress = require('aws-serverless-express')
 
 const shortid = require('shortid');
 const DEFAULT_DURATION_SECONDS: number = 10;
 
+const expressReceiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
 const app = new App({
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    receiver: expressReceiver,
     token: process.env.SLACK_BOT_TOKEN
 });
+
+const server = awsServerlessExpress.createServer(expressReceiver.app);
 
 function generateDuration(duration, defaultDuration) {
     return !duration || isNaN(parseInt(duration)) ? defaultDuration : duration;
@@ -50,13 +58,19 @@ app.command('/audio', async ({payload, ack, say}) => {
     } catch (err) {
         console.log("Something went wrong: " + err)
     }
-
-
 });
 
 
 // Start your app
-(async () => {
-    await app.start(process.env.PORT || 4000);
-    console.log('⚡️ Bolt app is running!');
-})();
+//(async () => {
+//    await app.start(process.env.PORT || 4000);
+//    console.log('⚡️ Bolt app is running!');
+//})();
+
+// Handle the Lambda function event
+module.exports.handler = (event, context) => {
+  console.log('⚡️ Bolt app is running!');
+  awsServerlessExpress.proxy(server, event, context);
+};
+
+
